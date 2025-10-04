be9d86e217142987367527a50ac1761219e40740
Bùi Quang Huy <huybuihc2292005@gmail.com>
2025-10-04 23:30:34 +0700

    sửa

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
