43a6ba21a086ed7fb0bdd6b043f7f4647fbb080c
Bùi Quang Huy <huybuihc2292005@gmail.com>
2025-10-04 23:09:57 +0700

    xoa welcome

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
